04afdafa460e76b5b202364125f8bc6551a86207
MochHelmiHasan6 <70736429+MochHelmiHasan6@users.noreply.github.com>
2021-03-10 10:43:20 +0700

    KonversiBinaryToDecimal

---
+++ lib/input.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Input extends StatelessWidget {
+  const Input({
+    Key key,
+    @required this.etDecimal,
+  }) : super(key: key);
+
+  final TextEditingController etDecimal;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: etDecimal,
+      decoration: const InputDecoration(hintText: "Masukan Bilangan"),
+      keyboardType: TextInputType.number,
+      inputFormatters: <TextInputFormatter>[
+        FilteringTextInputFormatter.digitsOnly
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'input.dart';
+import 'result.dart';
+import 'convert.dart';
+import 'history.dart';
+import 'dropdown.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+// This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController etDecimal = new TextEditingController();
+
+  int nDecimal = 0;
+  int i = 1;
+  String _valueFrom = "Decimal";
+  String _valueTo = "Binary";
+  int _result = 0;
+  List<String> listViewItem = [];
+
+  void _konversiBilangan() {
+    setState(() {
+      nDecimal = int.parse(etDecimal.text);
+      if (_valueFrom == "Decimal") {
+        if (_valueTo == "Binary") {
+          int nBiner = 0;
+          while (nDecimal > 0) {
+            nBiner = (nDecimal % 2) * i;
+            i = i * 10;
+          }
+          _result = nBiner;
+        } else if (_valueTo == "Hexadecimal") {
+        } else if (_valueTo == "Octal") {}
+      } else if (_valueFrom == "Binary") {
+        if (_valueTo == "Decimal") {
+        } else if (_valueTo == "Hexadecimal") {
+        } else if (_valueTo == "Octal") {}
+      } else if (_valueFrom == "Hexadecimal") {
+        if (_valueTo == "Binary") {
+        } else if (_valueTo == "Decimal") {
+        } else if (_valueTo == "Octal") {}
+      } else if (_valueFrom == "Octal") {
+        if (_valueTo == "Binary") {
+        } else if (_valueTo == "Hexadecimal") {
+        } else if (_valueTo == "Decimal") {}
+      }
+      listViewItem.insert(0, "$_valueTo : $_result");
+    });
+  }
+
+  void dropdownOnChanged(String changeValue) {
+    setState(() {
+      _valueFrom = changeValue;
+    });
+  }
+
+  void dropdownOnChanged2(String changeValue) {
+    setState(() {
+      _valueTo = changeValue;
+    });
+    _konversiBilangan();
+  }
+
+  var listString = [
+    "Binary",
+    "Decimal",
+    "Hexadecimal",
+    "Octal",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Konverter Bilangan"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Input(etDecimal: etDecimal),
+              Container(
+                margin: EdgeInsets.only(top: 10, bottom: 10),
+                child: Text(
+                  "Dari Bilangan",
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+              Dropdown(
+                listView: listString,
+                newValue: _valueFrom,
+                newMethod: dropdownOnChanged,
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10, bottom: 10),
+                child: Text(
+                  "Konversi ke Bilangan",
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+              Dropdown(
+                listView: listString,
+                newValue: _valueTo,
+                newMethod: dropdownOnChanged2,
+              ),
+              Result(
+                result: _result,
+              ),
+              Convert(konvertHandler: _konversiBilangan),
+              Container(
+                margin: EdgeInsets.only(top: 10, bottom: 10),
+                child: Text(
+                  "Riwayat Konversi",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Expanded(
+                child: RiwayatKonversi(listViewItem: listViewItem),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
